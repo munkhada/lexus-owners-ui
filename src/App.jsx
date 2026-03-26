2eb92e2f978e27c24dc4a3ce4fdade2f2bb8e880
munkhada <munkhada.toyota@gmail.com>
2026-03-26 17:19:49 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 }
