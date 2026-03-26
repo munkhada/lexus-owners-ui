50fd1866b770bfee821ad1892c459b2ebfa9e655
munkhada <munkhada.toyota@gmail.com>
2026-03-26 17:16:43 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+
 export default function App() {
   return (
-    <div style={{ color: "red", fontSize: "60px" }}>
-      APP TEST
-    </div>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
   );
 }
